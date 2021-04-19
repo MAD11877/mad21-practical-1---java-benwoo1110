a019500681ab03990b068ccd16e9fc2f44e394b6
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-04-19 12:15:57 +0800

    Submit answers!

--- Question4.java
+++ Question4.java
@@ -18,6 +18,13 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int lines = in.nextInt();
+    for (int i = 0; i < lines; i++) {
+      StringBuilder sb = new StringBuilder();
+      for (int x = 0; x < (lines - i); x++) {
+        sb.append('*');
+      }
+      System.out.println(sb);
+    }
   }
 }
